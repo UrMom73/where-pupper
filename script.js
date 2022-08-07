9afe87826dbe0c40828511ba73f64717af77335c
UrMom73 <rajbirlongia19@gmail.com>
2022-08-07 00:03:12 -0700

    improved js

--- script.js
+++ script.js
@@ -5,8 +5,181 @@ let giveInput = true;
 let inputPlaceholder = true;
 let typeSpeed = 40;
 let gameText = document.createElement('div');
-
 gameText.id = 'gameText';
+
+const player = {
+    _location: 'house',
+    _stamina: 10,
+    _visited: ['house'],
+    _asked: [],
+    _inventory: [],
+
+    //getters and setters
+    get location () {
+        if(typeof this._location === 'string') {
+            return this._location;
+        };
+        console.log('Error: player.location is an unexpected value');
+    },
+
+    set location (val) {
+        if(typeof val === 'string') {
+            this._location = val;
+        }else {
+            console.log('Error: player.location must be a string');
+        };
+    },
+
+    get visited () {
+        if(Array.isArray(this._visited)) {
+            return this._visited;
+        };
+        console.log('Error: player.visited is an unexpected value');
+    },
+
+    set visited (val) {
+        if(typeof val === 'string') {
+            this.visited.push(val);
+        }else {
+            console.log('Error: player.visited contains string only');
+        };
+    },
+
+    get asked () {
+        if(Array.isArray(this._asked)) {
+            return this._asked;
+        };
+        console.log('Error: player.asked is an unexpected value');
+    },
+
+    set asked (val) {
+        if(typeof val === 'string') {
+            this.asked.push(val);
+        }else {
+            console.log('Error: player.asked contains string only');
+        };
+    },
+
+    get inv () {
+        if(Array.isArray(this._inventory)) {
+            return this._inventory;
+        };
+        console.log('Error: player.inv is an unexpected value');
+    },
+
+    get invList () {
+        return this.inv.join(', ');
+    },
+
+    set inv (val) {
+        if(typeof val === 'string') {
+            this.inv.push(val);
+        }else {
+            console.log('Error: player.inv contains string only');
+        };
+    },
+
+    useStamina (place) {
+        let distance = map.calcDist(map.places[player.location].id - map.places[place].id);
+        this.stamina -= Math.round(Math.random() * ((distance+1) - (distance-1)) + (distance-1));
+    }
+
+};
+
+//location class
+class Place {
+    constructor(id, objects, people) {
+        this._id = id;
+        this._objects = objects;
+        this._people = people;
+    };
+
+    get id () {
+        if(typeof this._id === 'number') {
+            return this._id;
+        };
+        console.log('Error: id is an unexpected value');
+    };
+
+    get objects () {
+        if(Array.isArray(this._objects)) {
+            return this._objects;
+        };
+        console.log('Error: objects is an unexpected value');
+    };
+
+    get people () {
+        if(Array.isArray(this._people)) {
+            return this._people;
+        };
+        console.log('Error: people is an unexpected value');
+    };
+
+    hasObj (obj) {
+        return this.objects.includes(obj);
+    };
+
+    hasGuy (guy) {
+        return this.people.includes(guy);
+    };
+};
+
+class Person {
+    constructor(type, name, mood) {
+        this._type = type;
+        this._name = name;
+        this._mood = mood;
+    };
+
+    get type () {
+        if(typeof this._type === 'string') {
+            return this._type;
+        };
+        console.log('Error: type is an unexpected value');
+    };
+
+    get name () {
+        if(typeof this._name === 'string') {
+            return this._name;
+        };
+        console.log('Error: name is an unexpected value');
+    };
+
+    get mood () {
+        if(typeof this._mood === 'number') {
+            return this._mood;
+        };
+        console.log('Error: mood is an unexpected value');
+    };
+
+    changeMood (amount) {
+        this.mood += amount;
+    };
+};
+
+const map = {
+    places: {
+        house: new Place(0, ['note'], []),
+        market: new Place(1, [], [new Person('cabbage man', 'Diego', 7)]),
+        park: new Place(-1, [], [new Person('old man', 'Bilal', 5)]),
+    },
+
+    calcDist (place1, place2) {
+        return Math.abs(place1 - place2);
+    },
+
+    get nearby () {
+        let locations = [];
+        for(let place in this.places) {
+            if(this.calcDist(this.places[player.location].id, this.places[place].id) === 1) {
+                locations.push(place);
+            };
+        };
+        return locations;
+    },
+
+    //consider keeping some places a secret
+};
 
 //menu popup functions
 function openWindow(windowName) {
@@ -17,59 +190,61 @@ function exitWindow(windowName) {
     document.getElementById(windowName).style.display = 'none';
 };
 
-//audio functions
-function audioFunction() {
+//settings
+function settings() {
     let musicFiles = document.getElementById('music').children;
     let sfxFiles = document.getElementById('sfx').children;
+    let userSpeed = document.getElementById('typeSpeed');
 
     //for music
     if (document.getElementById('musicSelect').value === 'off') {
-        for (let i = 0; i < musicFiles.length; i++) {
-            musicFiles[i].muted = true;
-        };
+        [...musicFiles].forEach(file => file.muted = true);
 
     } else {
-        for (let i = 0; i < musicFiles.length; i++) {
-            musicFiles[i].muted = false;
-        };
+        [...musicFiles].forEach(file => file.muted = false);
     };
     //for sound effects
     if (document.getElementById('sfxSelect').value === 'off') {
-        for (let i = 0; i < sfxFiles.length; i++) {
-            sfxFiles[i].muted = true;
-        };
+        [...sfxFiles].forEach(file => file.muted = true);
 
     } else {
-        for (let i = 0; i < sfxFiles.length; i++) {
-            sfxFiles[i].muted = false;
-        };
+        [...sfxFiles].forEach(file => file.muted = false);
+    };
+    //for type speed
+    if (!userSpeed.value) {
+        userSpeed.value = 40;
+
+    }else if(userSpeed.value < userSpeed.getAttribute('min')) {
+        userSpeed.value = userSpeed.getAttribute('min');
+        typeSpeed = parseInt(userSpeed.getAttribute('min'));
+
+    }else if(userSpeed.value > userSpeed.getAttribute('max')) {
+        userSpeed.value = userSpeed.getAttribute('max');
+        typeSpeed = parseInt(userSpeed.getAttribute('max'));
+
+    }else {
+        typeSpeed = parseInt(userSpeed.value);
     };
 };
 
 //button hover sound effect
-let buttons = document.querySelectorAll('button');
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].onmouseover = buttonBeep;
-};
+[...document.querySelectorAll('button')].forEach(button => button.onmouseover = buttonBeep);
 
 function buttonBeep() {
     document.getElementById('buttonBeep').play();
     if(!document.getElementById('bg-music').paused) {
         return;
-
-    }else {
-        document.getElementById('bg-music').play();
-    }
+    };
+    document.getElementById('bg-music').play();
 };
 
 //frustrating but for chrome, autoplay doesn't work, so gotta play the music somehow
 function buttonClick() {
     if(!document.getElementById('bg-music').paused) {
         return;
+    };
 
-    }else {
-        document.getElementById('bg-music').play();
-    }
+    document.getElementById('bg-music').play();
 };
 
 //start game functions
@@ -89,68 +264,58 @@ function eraseMenu() {
 
 function startGame() {
     document.getElementById('alarm').pause();
-    textGenerator(`You groan in defiance and hit snooze on your alarm. Pupper will probably wake you up anyway. But as you lie there, something feels off. Confused, you get up and step out into the hallway. The house is unusually quiet. "Pupper?" you call out. "O Pupper! Where art thou Pupper?" But there's no response. Then you see it: lying by the front door is Pupper's favorite toy along with his collar. Next to it is your mama's favorite vase, smashed into pieces! Someone must've kidnapped Pupper! You need to find him and make whoever did this pay. ${listLocation()}`);
+    textGenerator(`You groan in defiance and hit snooze on your alarm. Pupper will probably wake you up anyway. But as you lie there, something feels off. Confused, you get up and step out into the hallway. The house is unusually quiet. "Pupper?" you call out. "O Pupper! Where art thou Pupper?" But there's no response. Then you see it: lying by the front door is Pupper's favorite toy along with his collar. Next to it is your mama's favorite vase, smashed into pieces! Someone must've kidnapped Pupper! You need to find him and make whoever did this pay.`);
 };
 
 //text generator takes string parameter, splits into individual characters
 function textGenerator(string) {
-    gameText.innerHTML += `<div></div>`
+    let div = document.createElement('div');
+    gameText.appendChild(div);
+
     let stringArray = string.split('');
     let numOfHighlights = 0;
 
-    for(let i = 0; i < stringArray.length; i++) {
-        if(stringArray[i] === '{') {
+    stringArray.forEach(elem => {
+        if(elem === '{') {
             numOfHighlights++;
         };
-    };
 
-    for (let i = 0; i < stringArray.length; i++) {
-        gameText.lastElementChild.innerHTML += `<span>${stringArray[i]}</span>`;
-    };
+        let span = document.createElement('span');
+        span.innerHTML = elem;
+
+        div.appendChild(span);
+    });
 
     //highlights text if important to player's knowledge
     function highlighter() {
         if(numOfHighlights === 0) {
             return;
-        }else {
-            let spanArray = gameText.lastElementChild.children;
-            let highlightStart;
-            for(let i = 0; i < spanArray.length; i++) {
-                if(spanArray[i].innerHTML === '{') {
-                    highlightStart = i;
-                    break;
-                };
-            };
+        };
+
+        let spanArray = gameText.lastElementChild.children;
+        let highlightStart = [...spanArray].findIndex(elem => elem.innerHTML === '{');
+        let highlightEnd = [...spanArray].findIndex(elem => elem.innerHTML === '}');
     
-            let highlightEnd;
-            for(let i = 0; i  < spanArray.length; i++) {
-                if(spanArray[i].innerHTML === '}') {
-                    highlightEnd = i;
-                    break;
-                };
-            };
+        for(let i = highlightStart; i < highlightEnd; i++) {
+            spanArray[i].classList.add('highlighter');
+        };
     
-            for(let i = highlightStart; i < highlightEnd; i++) {
-                spanArray[i].classList.add('highlighter');
-            };
-    
-            spanArray[highlightEnd].remove();
-            spanArray[highlightStart].remove();
+        spanArray[highlightEnd].remove();
+        spanArray[highlightStart].remove();
             
-            numOfHighlights--;
-        };
-    
-        if(numOfHighlights !== 0) {
-            highlighter();
-        };
+        numOfHighlights--;
+        
+        highlighter();
     };
 
-    highlighter();
+    if(numOfHighlights > 0) {
+        highlighter();
+    }
 
     document.getElementById('typingSound').play();
     timer = setInterval(onTick, typeSpeed);
     window.scrollTo(0, gameText.scrollHeight);
-}
+};
 
 //onTick styles individual span characters
 function onTick() {
@@ -174,21 +339,19 @@ function onTick() {
 gameText.addEventListener('dblclick', function() {
     if(gameText.lastElementChild.tagName === 'INPUT' || gameText.lastElementChild.tagName === 'SPAN') {
         return;
-
-    }else {
-        clearInterval(timer);
-        let array = gameText.lastElementChild.children;
-        while(char < array.length) {
-            array[char].classList.add('typewriter');
-            char++;
-        };
-        onTick();
     };
+
+    clearInterval(timer);
+    let array = gameText.lastElementChild.children;
+    while(char < array.length) {
+        array[char].classList.add('typewriter');
+        char++;
+    };
+    onTick();
 });
 
 //generates input for player
 function inputGenerator() {
-    // let gameText = document.getElementById('gameText');
     gameText.innerHTML += `<span class="typewriter">&gt;</span> <input type="text">`;
     let input = gameText.lastElementChild;
 
@@ -197,7 +360,8 @@ function inputGenerator() {
         inputPlaceholder = false;
     };
 
-    input.setAttribute('maxlength', '30')
+    input.setAttribute('maxlength', '30');
+    input.focus();
 
     input.addEventListener('keydown', function (key) {
         if (key.keyCode === 13) {
@@ -211,11 +375,10 @@ function inputGenerator() {
 function masterFunction(playerResponse) {
     
     gameText.lastElementChild.outerHTML = `<span class="typewriter playerResponse">${playerResponse}</span>`;
-
+    
     playerResponse = playerResponse.trim().toLowerCase().split(' ');
-
-    let action = playerResponse[0];
-    playerResponse.splice(0, 1);
+    
+    let action = playerResponse.shift();
 
     let object = playerResponse.join(' ');
 
@@ -228,7 +391,7 @@ function masterFunction(playerResponse) {
     }else if(action === 'unmute') {
         unmute(object);
     
-    }else if(action === 'search') {
+    }else if(action === 'search' || action === 'investigate') {
         search(object);
     
     }else if(action === 'grab' || action === 'collect' || action === 'take') {
@@ -241,12 +404,8 @@ function masterFunction(playerResponse) {
         inspect(object);
     
     }else if(action === 'inventory' || action === 'backpack') {
-        if(player.inventory.length === 0) {
-            textGenerator('There is nothing in your inventory.');
+        textGenerator('Undergoing construction');
 
-        }else {
-            textGenerator(`In your inventory: {${player.inventory.join(', ')}}. To use, type {use}.`);
-        };
     }else if(action === 'use') {
         use(object);
     
@@ -259,16 +418,12 @@ function masterFunction(playerResponse) {
 function mute(sound) {
     if(sound === 'music') {
         let musicFiles = document.getElementById('music').children;
-        for(let i = 0; i < musicFiles.length; i++) {
-            musicFiles[i].muted = true;
-        };
+        [...musicFiles].forEach(file => file.muted = true);
         textGenerator('Music has been muted.');
     
     }else if(sound === 'sfx') {
         let sfxFiles = document.getElementById('sfx').children;
-        for(let i = 0; i < sfxFiles.length; i++) {
-            sfxFiles[i].muted = true;
-        };
+        [...sfxFiles].forEach(file => file.muted = true);
         textGenerator('Sfx have been muted.');
         
     }else {
@@ -280,16 +435,12 @@ function mute(sound) {
 function unmute(sound) {
     if(sound === 'music') {
         let musicFiles = document.getElementById('music').children;
-        for(let i = 0; i < musicFiles.length; i++) {
-            musicFiles[i].muted = false;
-        };
+        [...musicFiles].forEach(file => file.muted = false);
         textGenerator('Music has been unmuted.');
         
     }else if(sound === 'sfx') {
         let sfxFiles = document.getElementById('sfx').children;
-        for(let i = 0; i < sfxFiles.length; i++) {
-            sfxFiles[i].muted = false;
-        };
+        [...sfxFiles].forEach(file => file.muted = false);
         textGenerator('Sfx have been unmuted.');
         
     }else {
@@ -297,68 +448,15 @@ function unmute(sound) {
     };
 };
 
-//list available locations
-function listLocation() {
-    let nearbyLocations = [];
-    for(const placeName in places) {
-        if(Math.abs(places[placeName].location - player.location) === 1) {
-            nearbyLocations.push(placeName);
-        };
-    }
-
-    return `Places nearby: {${nearbyLocations.join(', ')}}.`;
-
-};
-
-//list available objects
-function listObjects(location) {
-    if(places[location].objects.length > 0) {
-        return `In this area, you see {${places[location].objects.join(', ')}}.`;
-
-    }else {
-        return '';
-
-    };
-};
-
-const peopleList = ['vender'];
 
 //search function, player moves to other areas if nearby
 function search(place) {
     if(place.trim().length === 0) {
         textGenerator('Clarify which place you want to search.');
     
-    //if inputted location doesn't exist or isn't nearby
-    }else if(places.exists(place) === false || Math.abs(places[place].location - player.location) > 1) {
-        textGenerator(`You look around for "${place}" but can't see it anywhere. ${listLocation()}`);
-    
-    }else if(place === 'house') {
-
-
-    }else if(place === player.previousLocation) {
-        textGenerator(`You're already in that location. ${listLocation()}`);
-
-    }else if(place === 'market') {
-        player.location = 1;
-        if(player.placesVisited.includes('market') === false) {
-            player.placesVisited.push('market');
-            if(player.previousLocation === 'house') {
-                textGenerator(`You exit the house and make your way to the market. There, you see hundreds of people buying and selling goods. As you walk along the narrow path between the vending stalls, you notice a vender selling fruit. ${listObjects('market')} ${listLocation()}`);
-
-            }
-
-        }else if(player.placesVisited.includes('market') === true) {
-            if(player.previousLocation === 'house') {
-                textGenerator(`You exit the house and make your way to the market. You see the fruit vender doing business as usual. ${listObjects('market')}`);
-
-            }
-        }
-        player.previousLocation = 'market';
-
-    }else if(place === 'park') {
-        textGenerator('Still in development!');
-
-    }
+    }else {
+        textGenerator('In development');
+    };
 };
 
 //grab function, player equips objects if nearby
@@ -371,9 +469,8 @@ function ask(person) {
     if(person.trim().length === 0) {
         textGenerator('Clarify who you want to ask.');
 
-    }else if(peopleList.includes(person) === false || places[player.previousLocation].people !== person) {
-        textGenerator(`You look around for "${person}" but can't see them anywhere.`);
-
+    }else {
+        textGenerator('Still in development!');
     }
 };
 
@@ -385,46 +482,4 @@ function inspect(object) {
 //use function, player uses objects in inventory
 function use(object) {
     textGenerator('Still in development!');
-}
-
-//objects
-const player = {
-    inventory: [],
-    placesVisited: [],
-    peopleAsked: [],
-    location: 0,
-    previousLocation: 'house'
-};
-
-const places = {
-    house: {
-        location: 0,
-        objects: []
-    },
-
-    market: {
-        location: 1,
-        objects: [],
-        people: 'vendor'
-    },
-
-    park: {
-        location: -1,
-        objects: []
-    },
-
-    metro: {
-        location: -2,
-        objects: []
-    },
-
-    exists(place) {
-        if(this[place] === undefined) {
-            return false;
-        }else {
-            return true;
-        };
-    },
-
-
 }
